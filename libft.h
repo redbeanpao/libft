e90f7b5d7522f7ecbd0e9ec92f2112f5e08fe797
Hui Zhen Leong <hleong@u81z01s05.42kl.edu.my>
2022-12-08 21:30:08 +0800

    08-Dec Update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: hleong <hleong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 01:21:42 by hleong            #+#    #+#             */
-/*   Updated: 2022/12/08 19:25:00 by hleong           ###   ########.fr       */
+/*   Updated: 2022/12/08 21:22:57 by hleong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
